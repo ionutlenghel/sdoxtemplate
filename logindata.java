d0eaa6c564ef9257a7ad082e25d834745c3bd48e
ionutlenghel <48522812+ionutlenghel@users.noreply.github.com>
2019-03-13 16:39:19 +0200

    Create logindata.java

---
+++ logindata.java
@@ -0,0 +1,29 @@
+package myres_q;
+
+import org.testng.annotations.DataProvider;
+
+public class logindata {
+	
+	@DataProvider(name="logindataAdmin")
+    public Object [][] checkoutData() {
+        Object[][] obj = new Object[1][5];
+        
+            obj[0][0]="xxxx";
+            obj[0][1]="xxxx";
+            obj[0][2]="He determined to drop his litigation with the monastry";
+            obj[0][3]="In 30 years of multiThreaded app development";
+            obj[0][4]="none really";
+            
+        return obj;
+    }
+	
+	@DataProvider(name="sdox")
+    public Object [][] sdox() {
+        Object[][] obj = new Object[1][2];
+        
+            obj[0][0]="xxxx";
+            obj[0][1]="xxxx";
+            
+        return obj;
+    }
+}
