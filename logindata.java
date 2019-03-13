7d2ba1f778ab444db0c34f38b9cecab843b0d624
ionutlenghel <48522812+ionutlenghel@users.noreply.github.com>
2019-03-13 16:49:27 +0200

    Update logindata.java

--- logindata.java
+++ logindata.java
@@ -8,8 +8,8 @@ public class logindata {
     public Object [][] checkoutData() {
         Object[][] obj = new Object[1][5];
         
-            obj[0][0]="xxxx";
-            obj[0][1]="xxxx";
+            obj[0][0]="email";
+            obj[0][1]="password";
             obj[0][2]="He determined to drop his litigation with the monastry";
             obj[0][3]="In 30 years of multiThreaded app development";
             obj[0][4]="none really";
@@ -21,8 +21,8 @@ public class logindata {
     public Object [][] sdox() {
         Object[][] obj = new Object[1][2];
         
-            obj[0][0]="xxxx";
-            obj[0][1]="xxxx";
+            obj[0][0]="email";
+            obj[0][1]="password";
             
         return obj;
     }
